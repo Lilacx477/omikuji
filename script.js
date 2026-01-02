e5582ce0d2f883bbe53ab7dd34d09fbd70edea2e
Lilacx477 <90629204+Lilacx477@users.noreply.github.com>
2026-01-02 14:31:38 +0900

    Refactor hashstring and drawomikuji functions

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-function hashstring(str) {
-	let hash=0;
-	for (let i=0; i < str.length; i++) {
-		hash = (hash << 5) - hash + str.charCodeAt(i);
-		hash |= 0;
-		}
-		return Math.abs(hash);
-	}
-	
-const omikujilist = [
-	{name:"激震", image:"Gekishin.png"},
-	{name:"強震", image:"Kyoshin.png"},
-	{name:"弱震", image:"Jakushin.png"},
-	{name:"水中を寝正月の影がよぎった", image:"Tyokkan.png"}
+function hashString(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash << 5) - hash + str.charCodeAt(i);
+    hash |= 0;
+  }
+  return Math.abs(hash);
+}
+
+const omikujiList = [
+  { name: "激震", image: "Gekishin.png" },
+  { name: "強震", image: "Kyoshin.png" },
+  { name: "弱震", image: "Jakushin.png" },
+  { name: "水中を寝正月の影がよぎった", image: "Tyokkan.png" }
 ];
 
-function drawomikuji() {
-	const seed = document.getElrmentById("seedinput").value;
-	if (!seed) return;
-	
-	const hash = hashstring(seed);
-	const index =hash%omikujilist.length;
-	const result = omikujilist[index];
-	
-	document.getElementById("resultrext"),textContent=result.name;
-	document.getElementById("resultimage").src=result.image;
+function drawOmikuji() {
+  const seed = document.getElementById("seedinput").value;
+  if (!seed) return;
+
+  const hash = hashString(seed);
+  const index = hash % omikujiList.length;
+  const result = omikujiList[index];
+
+  document.getElementById("resulttext").textContent = result.name;
+  document.getElementById("resultimage").src = result.image;
 }
